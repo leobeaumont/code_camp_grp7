84eebf5621ae7c3a7067d9f84472b63600fdd292
Lucie Desnoyers <lucie.desnoyers@imt-atlantique.net>
2025-09-08 16:50:28 +0200

    fix fautes d'orthographe

--- parser.py
+++ parser.py
@@ -10,19 +10,22 @@ def create_parser():
   # Ajout d’un sous-parseur pour les sous-commandes
   subparsers = parser.add_subparsers(help='The commands to manage orders', dest='command', required=True)
 
-  # Création du parseur pour la commandeadd
+  # Création du parseur pour la commande add
   parser_add = subparsers.add_parser ('add', help='Add a new task.')
   parser_add.add_argument('details', nargs='*', default="no details",help="task details")
 
-  # Création du parseur pour la commandemodify
+  # Création du parseur pour la commande modify
   parser_modify = subparsers.add_parser('modify',help='Modify a task givenits id')
   parser_modify.add_argument('id', help="the task id")
   parser_modify.add_argument('details', nargs='*', default="no details", help="the new details")
 
-  # Création du parseur pour la commanderm
-  parser_rm = subparsers.add_parser('rm', help='Remove an task given its id')
+  # Création du parseur pour la commande rm
+  parser_rm = subparsers.add_parser('rm', help='Remove a task given its id')
   parser_rm.add_argument('id', help="the task id")
- 
-  # Création du parseur pour la commandeshow
+
+  # Création du parseur pour la commande show
   parser_show = subparsers.add_parser('show', help='Show the tasks')
   return parser
+
+
+
