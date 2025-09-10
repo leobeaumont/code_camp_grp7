f31d083f6612cfef9847f7ba95cb39910f65e4f1
Lucie Desnoyers <lucie.desnoyers@imt-atlantique.net>
2025-09-10 16:17:27 +0200

    changement id

--- commands.py
+++ commands.py
@@ -5,7 +5,7 @@ def get_id(file):
         return 0
     with open(file, 'r') as f:
         content = f.read()
-    return len(content.split("\n"))
+    return max([int(line.split(",")[0]) for line in content.split("\n") if line])
 
 def add_task(file, description):
     exist=os.path.isfile(file)
