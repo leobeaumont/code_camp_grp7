28e20900d81a68483d272325824ef56fb1617650
leobeaumont <leowhite3005@gmail.com>
2025-09-10 14:24:09 +0200

    Ajout de la fonction pour afficher les taches d'un fichier

--- commands.py
+++ commands.py
@@ -7,3 +7,9 @@ def remove_task(file, id):
         for line in file_text:
             if int(line.split(",")[0]) != id:
                 f.write(line)
+
+def show_tasks(file):
+    with open(file, "r") as f:
+        for line in f.readlines():
+            attr = line.strip("\n").split(",")
+            print("ID: {}  |  Desc: {}".format(attr[0], attr[1]))
